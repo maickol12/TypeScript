eb874c6e5fb886665fe34a91be7a8c65a1b54c80
maickol12 <mrodriguez@telstock.net>
2022-12-06 12:56:29 -0600

    update

---
+++ express-api/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.status(401).json({
+    ok: true,
+    msg:'Hello World!'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
